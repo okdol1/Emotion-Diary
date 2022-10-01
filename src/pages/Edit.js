199da092ff0dcc64a9787fcc5631d934728c7eb4
okdol1 <okdol0611@gmail.com>
2022-10-02 03:06:09 +0900

    React Router V6 커스텀훅 연습

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,35 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [serchParams, setSearchParams] = useSearchParams();
+
+  const id = serchParams.get("id");
+  console.log(id);
+
+  const mode = serchParams.get("mode");
+  console.log(mode);
   return (
     <div>
       <h1>Edit</h1>
       <p>아곳은 일기 수정 페이지입니다.</p>
+      <button onClick={() => setSearchParams({ who: "okdol" })}>
+        QS 바꾸기
+      </button>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        HOME으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
+      </button>
     </div>
   );
 };
