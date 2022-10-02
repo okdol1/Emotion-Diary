96b510f0043b723cab62ab953f018c8a4b79e2fa
okdol1 <okdol0611@gmail.com>
2022-10-03 01:32:35 +0900

    시간순 정렬 select 기능 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,36 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { DiaryStateContext } from "../App";
 
 import MyHeader from "./../components/MyHeader";
 import MyButton from "./../components/MyButton";
+import DiaryList from "../components/DiaryList";
 
 const Home = () => {
+  const diaryList = useContext(DiaryStateContext);
+
+  const [data, setData] = useState([]);
   const [curDate, setCurDate] = useState(new Date());
   const headText = `${curDate.getFullYear()}년 ${curDate.getMonth(+1)}월`;
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const firstDate = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth(),
+        1
+      ).getTime(); // 이번년도 이번월의 1일
+
+      const lastDate = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth() + 1,
+        0
+      ).getTime(); // 이번년도 이번월의 마지막 날짜
+
+      setData(
+        diaryList.filter((it) => firstDate <= it.date && it.date <= lastDate)
+      );
+    }
+  }, [diaryList, curDate]);
 
   const increaseMonth = () => {
     setCurDate(
@@ -25,6 +50,7 @@ const Home = () => {
         leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
         rightChild={<MyButton text={">"} onClick={increaseMonth} />}
       />
+      <DiaryList diaryList={data} />
     </div>
   );
 };
