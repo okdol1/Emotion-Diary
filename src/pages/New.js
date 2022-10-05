69c493efc7d246eb25848cbe1e1604396bf8b81e
okdol1 <okdol0611@gmail.com>
2022-10-05 23:51:49 +0900

    새 일기쓰기 헤더

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
+import MyHeader from "./../components/MyHeader";
+import MyButton from "./../components/MyButton";
+
 const New = () => {
+  const navigete = useNavigate();
   return (
     <div>
-      <h1>New</h1>
-      <p>아곳은 일기 작성 페이지입니다.</p>
+      <MyHeader
+        headText={"새 일기쓰기"}
+        leftChild={
+          <MyButton text={"< 뒤로가기"} onClick={() => navigete(-1)} />
+        }
+      />
     </div>
   );
 };
