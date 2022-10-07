6dd72dd41937e17934ce5defbd04939d52519794
okdol1 <okdol0611@gmail.com>
2022-10-07 14:58:10 +0900

    일기상세 컴포넌트 구현

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -5,35 +5,8 @@ import { DiaryDispatchContext } from "../App";
 import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
 import EmotionItem from "./EmotionItem";
-import { getStringDate } from "./../util/date";
-
-const emotionList = [
-  {
-    emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-    emotion_descript: "완전 좋음",
-  },
-  {
-    emotion_id: 2,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
-    emotion_descript: "좋음",
-  },
-  {
-    emotion_id: 3,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-    emotion_descript: "그럭저럭",
-  },
-  {
-    emotion_id: 4,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-    emotion_descript: "나쁨",
-  },
-  {
-    emotion_id: 5,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
-    emotion_descript: "끔찍함",
-  },
-];
+import { getStringDate } from "../util/date";
+import { emotionList } from "../util/emotion";
 
 const DiaryEditor = ({ isEdit, originData }) => {
   const contentRef = useRef();
