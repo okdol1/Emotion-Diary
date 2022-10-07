452613e73ac195db1a1b49607bc4e27c2c2b1a9e
okdol1 <okdol0611@gmail.com>
2022-10-07 13:57:13 +0900

    Edit 구현

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const reducer = (state, action) => {
     }
     case "EDIT": {
       newState = state.map((it) =>
-        it.id === action.data.id ? { ...action.data } : it
+        it.id === action.data.id ? action.data : it
       );
       break;
     }
@@ -117,7 +117,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/new" element={<New />} />
-              <Route path="/edit" element={<Edit />} />
+              <Route path="/edit/:id" element={<Edit />} />
               <Route path="/diary/:id" element={<Diary />} />
             </Routes>
           </div>
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,35 +1,36 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useContext, useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { DiaryStateContext } from "../App";
+import DiaryEditor from "../components/DiaryEditor";
 
 const Edit = () => {
+  const [originData, setoriginData] = useState();
   const navigate = useNavigate();
-  const [serchParams, setSearchParams] = useSearchParams();
+  const { id } = useParams();
+  // console.log(id)
 
-  const id = serchParams.get("id");
-  console.log(id);
+  const diaryList = useContext(DiaryStateContext);
+  // console.log(diaryList)
 
-  const mode = serchParams.get("mode");
-  console.log(mode);
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+      console.log(targetDiary);
+
+      if (targetDiary) {
+        setoriginData(targetDiary);
+      } else {
+        // 만약 일기가 5번까지 있는데 /edit/9로 이동했을경우 홈으로이동, 뒤로가지 못하게
+        navigate("/", { replace: true });
+      }
+    }
+  }, [id, diaryList]);
+
   return (
     <div>
-      <h1>Edit</h1>
-      <p>아곳은 일기 수정 페이지입니다.</p>
-      <button onClick={() => setSearchParams({ who: "okdol" })}>
-        QS 바꾸기
-      </button>
-      <button
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        HOME으로 가기
-      </button>
-      <button
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
-        뒤로가기
-      </button>
+      {originData && <DiaryEditor isEdit={true} originData={originData} />}
     </div>
   );
 };
